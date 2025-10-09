b879c403cb195b34e98417fadcf56e6dd965c609
moldovanluca <moldovanluca1605@gmail.com>
2025-10-09 19:53:50 +0200

    updated the building sql

--- ISA hierarchies implementation/Building.sql
+++ ISA hierarchies implementation/Building.sql
@@ -1,12 +1,16 @@
+--superclass
 CREATE TABLE Building (
     id INT PRIMARY KEY AUTO_INCREMENT,
     address_ VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE SSC (
+--subclasses
+CREATE TABLE SCC (
     building_id INT,
     club VARCHAR(50),
-    club_schedule VARCHAR(50),
+    club_schedule_days VARCHAR(50),
+    start_time TIME,
+    end_time TIME
     FOREIGN KEY (building_id) REFERENCES Building(id)
 );
 
@@ -14,6 +18,7 @@ CREATE TABLE IRC (
     building_id INT,
     books VARCHAR(50),
     book_availability BOOLEAN,
+    book_type VARCHAR(10),
     study_areas VARCHAR(50),
     study_area_capacity INT,
     other_rooms VARCHAR(50),
@@ -26,7 +31,9 @@ CREATE TABLE Gym (
     membership_price INT,
     equipment VARCHAR(50),
     personnel VARCHAR(50),
-    working_hours VARCHAR(50),
+    working_days VARCHAR(50),
+    start_time TIME,
+    end_time TIME,
     FOREIGN KEY (building_id) REFERENCES Building(id)
 );
 
@@ -34,7 +41,10 @@ CREATE TABLE Dorm (
     building_id INT,
     room_type VARCHAR(10),
     other_rooms VARCHAR(50),
-    servery_schedule VARCHAR(50),
+    meal_type VARCHAR(15),
+    servery_schedule_days VARCHAR(100),
+    start_time TIME,
+    end_time TIME,
     servery_menu VARCHAR(50),
     FOREIGN KEY (building_id) REFERENCES Building(id)
 );
@@ -43,14 +53,18 @@ CREATE TABLE Research_hall (
     building_id INT,
     department VARCHAR(50),
     rooms VARCHAR(50),
-    schedule VARCHAR(50),
+    schedule_days VARCHAR(50),
+    start_time TIME,
+    end_time TIME,
     FOREIGN KEY (building_id) REFERENCES Building(id)
 );
 
 CREATE TABLE RLH (
     building_id INT,
     services VARCHAR(50),
-    services_schedule VARCHAR(50),
+    services_schedule_days VARCHAR(50),
+    start_time TIME,
+    end_time TIME,
     services_office_number INT,
     other_room VARCHAR(50),
     FOREIGN KEY (building_id) REFERENCES Building(id)
